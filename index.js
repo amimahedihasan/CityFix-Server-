c4d11e6119e58606e4e177d2f0fcca7aabb64e88
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-05-18 07:02:10 +0600

    create role based authorization middleware

--- index.js
+++ index.js
@@ -12,3 +12,38 @@ app.use(express.json());
 
 
 const admin = require("firebase-admin");
+
+// const serviceAccount = require("./city-fix-firebase-adminsdk.json");
+const decoded = Buffer.from(process.env.FB_SERVICE_KEY, 'base64').toString('utf8')
+const serviceAccount = JSON.parse(decoded);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+
+const verifyFBToken = async (req, res, next) => {
+    const token = req.headers.authorization;
+    if (!token) {
+        return res.status(401).send({ message: 'Unauthorized access!' });
+    }
+    try {
+        const idToken = token.split(' ')[1];
+        const decoded = await admin.auth().verifyIdToken(idToken);
+        req.decoded_email = decoded.email;
+        next();
+    } catch (err) {
+        return res.status(401).send({ message: 'Unauthorized access!' });
+    }
+};
+
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASS}@cluster0.w0v9pwr.mongodb.net/?appName=Cluster0`;
+
+// MongoClient
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
