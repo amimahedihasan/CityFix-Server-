2f578461288f1b2b40832e01535fb627b1d64fc0
Md Mahedi Hasan <mdmahedi6427@gmail.com>
2026-05-18 07:05:30 +0600

    implement upvote functionality with restrictions

--- index.js
+++ index.js
@@ -184,23 +184,3 @@ async function run() {
             res.send({ updated: true, liked: !hasLiked });
         });
 
-
-        // Get single issue by ID
-        app.get('/issues/:id', async (req, res) => {
-            const id = req.params.id;
-            const issue = await issuesCollection.findOne({ _id: new ObjectId(id) });
-            res.send(issue);
-        });
-
-
-        // POST: Save User
-        app.post("/users", async (req, res) => {
-            const user = req.body;
-            const existing = await usersCollection.findOne({ email: user.email });
-            if (existing) {
-                return res.send({ message: "User already exists" });
-            }
-            const result = await usersCollection.insertOne(user);
-            res.send(result);
-        });
-
